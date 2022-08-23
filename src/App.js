3340b189fa0ed64c5c45d9043ee7d3c77c65198e
Glaykson Rodrigues <glaykson@gmail.com>
2022-08-23 10:26:38 -0300

    Resolução do requisito 7

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 // import { AppContext } from './context/AppContext';
 import Login from './components/Login';
+import Foods from './pages/Foods';
+import Drinks from './pages/Drinks';
+import DoneRecipes from './pages/DoneRecipes';
+import FavoriteRecipes from './pages/FavoriteRecipes';
+import Profile from './pages/Profile';
 
 function App() {
   // const { categories } = useContext(AppContext);
   return (
     <BrowserRouter>
-      <Route path="/" component={ Login } />
+      <Switch>
+        <Route path="/" exact component={ Login } />
+        <Route path="/foods" exact component={ Foods } />
+        <Route path="/drinks" exact component={ Drinks } />
+        <Route path="/profile" exact component={ Profile } />
+        <Route path="/done-recipes" exact component={ DoneRecipes } />
+        <Route path="/favorite-recipes" exact component={ FavoriteRecipes } />
+      </Switch>
     </BrowserRouter>
   );
 }
