45c92be88b7c90c779a2f7df35bf3fa0de3aa7fe
Glaykson Rodrigues <glaykson@gmail.com>
2022-08-23 21:45:14 -0300

    Resolução do requisito 9 (teste imcompleto)

--- src/__tests__/Header.test.js
+++ src/__tests__/Header.test.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import userEvent from '@testing-library/user-event';
 import { screen } from '@testing-library/react';
 import Header from '../components/Header';
 import renderWithRouter from '../utils/renderWithRouter';
@@ -10,5 +11,20 @@ describe('Teste do "Header"', () => {
     expect(screen.getByTestId('page-title')).toBeInTheDocument();
     expect(screen.getByTestId('profile-top-btn')).toBeInTheDocument();
     expect(screen.getByTestId('search-top-btn')).toBeInTheDocument();
+  });
+
+  it('Se os elementos estão sendo renderizados', () => {
+    renderWithRouter(<Header />);
+
+    expect(screen.queryByTestId('search-input')).not.toBeInTheDocument();
+    userEvent.click(screen.getByTestId('search-top-btn'));
+    expect(screen.queryByTestId('search-input')).toBeInTheDocument();
+  });
+
+  it('Se btn redireciona para profile', () => {
+    const { history } = renderWithRouter(<Header />);
+
+    userEvent.click(screen.getByTestId('profile-top-btn'));
+    expect(history.location.pathname).toEqual('/profile');
   });
 });
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import profileIcon from '../images/profileIcon.svg';
 import searchIcon from '../images/searchIcon.svg';
 
 function Header() {
+  const [searchArea, setSearchArea] = useState(false);
   const history = useHistory();
   const { location: { pathname } } = history;
   const conditionalArray = ['/profile', '/done-recipes', '/favorite-recipes'];
@@ -19,11 +20,17 @@ function Header() {
       <button type="button" onClick={ () => history.push('/profile') }>
         <img data-testid="profile-top-btn" src={ profileIcon } alt="profile Icone" />
       </button>
-      {!conditionalArray.includes(pathname) && <img
-        data-testid="search-top-btn"
-        src={ searchIcon }
-        alt="searcg Icone"
-      />}
+      {!conditionalArray.includes(pathname)
+      && (
+        <button type="button" onClick={ () => setSearchArea(!searchArea) }>
+          <img
+            data-testid="search-top-btn"
+            src={ searchIcon }
+            alt="searcg Icone"
+          />
+        </button>
+      )}
+      {searchArea && <input placeholder="Pesquisar" data-testid="search-input" />}
     </header>
   );
 }
