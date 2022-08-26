d8055b48874b2b0d9e4718251b4842d1e76b8724
João Gallo <joaogallop@gmail.com>
2022-08-26 15:00:02 -0300

    Adiciona tela de Profile

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,11 +1,54 @@
 import React from 'react';
+// import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
 import Header from '../components/Header';
+// import DoneRecipes from './DoneRecipes';
 
 export default function Profile() {
+  const email = localStorage.getItem('user');
+  // const history = useHistory();
+
+  // function handler(a) {
+  //   return history.push(`/${a}`);
+  // }
+
   return (
     <div>
       <Header />
-      Profile
+      <fieldset>
+        <p
+          type="email"
+          data-testid="profile-email"
+        >
+          {email}
+        </p>
+        <a href="/done-recipes">
+          <button
+            data-testid="profile-done-btn"
+            type="button"
+          >
+            Done Recipes
+          </button>
+        </a>
+        <a href="/favorite-recipes">
+          <button
+            data-testid="profile-favorite-btn"
+            type="button"
+          >
+            Favorite Recipes
+
+          </button>
+        </a>
+        <a href="/">
+          <button
+            data-testid="profile-logout-btn"
+            type="button"
+            onClick={ localStorage.clear() }
+          >
+            Logout
+
+          </button>
+        </a>
+      </fieldset>
     </div>
   );
 }
