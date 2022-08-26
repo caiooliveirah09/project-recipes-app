5b70f68c3a762bae3a32d5fefffff12d78529fe5
caiooliveirah09 <leelros@hotmail.com>
2022-08-26 18:05:58 -0300

    tests

--- src/__tests__/Profile.test.js
+++ src/__tests__/Profile.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import Profile from '../pages/Profile';
 import renderWithRouterAndContext from '../utils/helpers/renderWithRouterAndContext';
 
@@ -13,14 +14,33 @@ describe('test the Profile page', () => {
   afterEach(() => {
     localStorage.clear();
   });
-  test('1 - tests if it has the initial structures and gets the email', () => {
+  test('1 - tests if it has the initial structures', () => {
     const { history } = renderWithRouterAndContext(<Profile />, '/profile');
     expect(history.location.pathname).toBe('/profile');
+
     const pageTitle = screen.getByTestId('page-title');
     expect(pageTitle).toBeInTheDocument();
+
     const profileEmail = screen.getByTestId('profile-email');
     expect(profileEmail).toBeInTheDocument();
     expect(profileEmail).toHaveTextContent('{"email":"test@test"}');
-    // const error = screen.getByText('error');
+
+    const profileLogoutBtn = screen.getByTestId('profile-logout-btn');
+    userEvent.click(profileLogoutBtn);
+    expect(localStorage.user).toBeUndefined();
+  });
+  test('2 - test if the user email appears', () => {
+    renderWithRouterAndContext(<Profile />, '/profile');
+
+    const profileEmail = screen.getByTestId('profile-email');
+    expect(profileEmail).toBeInTheDocument();
+    expect(profileEmail).toHaveTextContent('{"email":"test@test"}');
+  });
+  test('3 - test if logging out clears localStorage', () => {
+    renderWithRouterAndContext(<Profile />, '/profile');
+
+    const profileLogoutBtn = screen.getByTestId('profile-logout-btn');
+    userEvent.click(profileLogoutBtn);
+    expect(localStorage.user).toBeUndefined();
   });
 });
