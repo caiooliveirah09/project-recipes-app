43760ac34926da0a680471f1612284c05e101053
caiooliveirah09 <leelros@hotmail.com>
2022-08-29 12:40:32 -0300

    c

--- src/pages/FavoriteRecipes.jsx
+++ src/pages/FavoriteRecipes.jsx
@@ -1,11 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from '../components/Header';
 
 export default function FavoriteRecipes() {
+  const [allRecipes] = useState(JSON
+    .parse(localStorage.getItem('favoriteRecipes')));
+  const [recipes, setRecipes] = useState(JSON
+    .parse(localStorage.getItem('favoriteRecipes')));
+
+  const filterAll = () => {
+    setRecipes(allRecipes);
+  };
+
+  const filterFoods = () => {
+    const favoriteFoods = allRecipes.filter((r) => r.type === 'food');
+    setRecipes(favoriteFoods);
+  };
+
+  const filterDrinks = () => {
+    const favoriteDrinks = allRecipes.filter((r) => r.type === 'drink');
+    setRecipes(favoriteDrinks);
+  };
+
   return (
     <div>
       <Header />
-      Favorite Recipes
+      <button
+        type="button"
+        data-testid="filter-by-all-btn"
+        onClick={ filterAll }
+      >
+        All
+      </button>
+      <button
+        type="button"
+        data-testid="filter-by-food-btn"
+        onClick={ filterFoods }
+      >
+        Foods
+      </button>
+      <button
+        type="button"
+        data-testid="filter-by-drink-btn"
+        onClick={ filterDrinks }
+      >
+        Drinks
+      </button>
+      { recipes.map((r, i) => (
+        <div key={ i }>
+          <img
+            data-testid={ `${i}-horizontal-image` }
+            src={ r.image }
+            alt={ r.name }
+          />
+        </div>))}
     </div>
   );
 }
