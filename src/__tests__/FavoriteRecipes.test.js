cbaf31f5f9a2b0db9b34260d1132bdf7fc50d46b
caiooliveirah09 <leelros@hotmail.com>
2022-08-29 17:51:52 -0300

    favorite recipes page ok

--- src/__tests__/FavoriteRecipes.test.js
+++ src/__tests__/FavoriteRecipes.test.js
@@ -1,13 +1,168 @@
 import React from 'react';
+import userEvent from '@testing-library/user-event';
 import { screen } from '@testing-library/react';
 import FavoriteRecipes from '../pages/FavoriteRecipes';
 import renderWithRouterAndContext from '../utils/helpers/renderWithRouterAndContext';
 
+const FILTERBYDRINKBTN = 'filter-by-drink-btn';
+const HORIZONTALNAME1 = '1-horizontal-name';
+const HORIZONTALNAME0 = '0-horizontal-name';
+
+const favoriteRecipes = [
+  {
+    id: '52977',
+    type: 'food',
+    nationality: 'Turkish',
+    category: 'Side',
+    alcoholicOrNot: '',
+    name: 'Corba',
+    image: 'https://www.themealdb.com/images/media/meals/58oia61564916529.jpg',
+  },
+  {
+    id: '15997',
+    type: 'drink',
+    nationality: '',
+    category: 'Ordinary Drink',
+    alcoholicOrNot: 'Optional alcohol',
+    name: 'GG',
+    image: 'https://www.thecocktaildb.com/images/media/drink/vyxwut1468875960.jpg',
+  },
+];
+
+const originalClipboard = { ...global.navigator.clipboard };
+
 describe('test the Favorite Recipes page', () => {
-  test('1 - IN CONSTRUCTION', () => {
+  beforeEach(() => {
+    localStorage.setItem('favoriteRecipes', JSON.stringify(favoriteRecipes));
+    const mockClipboard = {
+      writeText: jest.fn(),
+    };
+    global.navigator.clipboard = mockClipboard;
+  });
+
+  afterEach(() => {
+    localStorage.clear();
+    jest.resetAllMocks();
+    global.navigator.clipboard = originalClipboard;
+  });
+  test('1 - checks if it has the basic structure and filled in', () => {
     renderWithRouterAndContext(<FavoriteRecipes />);
-    const initialText = screen.getByText('Favorite Recipes');
-    expect(initialText).toBeInTheDocument();
-    // const error = screen.getByText('error');
+
+    const filterByAllBtn = screen.getByTestId('filter-by-all-btn');
+    expect(filterByAllBtn).toBeInTheDocument();
+
+    const filterByFoodBtn = screen.getByTestId('filter-by-food-btn');
+    expect(filterByFoodBtn).toBeInTheDocument();
+
+    const filterByDrinkBtn = screen.getByTestId(FILTERBYDRINKBTN);
+    expect(filterByDrinkBtn).toBeInTheDocument();
+
+    const horizontalImage0 = screen.getByTestId('0-horizontal-image');
+    expect(horizontalImage0).toBeInTheDocument();
+    expect(horizontalImage0).toHaveAttribute('src', 'https://www.themealdb.com/images/media/meals/58oia61564916529.jpg');
+
+    const horizontalTopText0 = screen.getByTestId('0-horizontal-top-text');
+    expect(horizontalTopText0).toBeInTheDocument();
+
+    const horizontalName0 = screen.getByTestId(HORIZONTALNAME0);
+    expect(horizontalName0).toBeInTheDocument();
+    expect(horizontalName0).toHaveTextContent('Corba');
+
+    const horizontalShareBtn0 = screen.getByTestId('0-horizontal-share-btn');
+    expect(horizontalShareBtn0).toBeInTheDocument();
+
+    const horizontalFavoriteBtn0 = screen.getByTestId('0-horizontal-favorite-btn');
+    expect(horizontalFavoriteBtn0).toBeInTheDocument();
+
+    const horizontalImage1 = screen.getByTestId('1-horizontal-image');
+    expect(horizontalImage1).toBeInTheDocument();
+    expect(horizontalImage1).toHaveAttribute('src', 'https://www.thecocktaildb.com/images/media/drink/vyxwut1468875960.jpg');
+
+    const horizontalTopText1 = screen.getByTestId('1-horizontal-top-text');
+    expect(horizontalTopText1).toBeInTheDocument();
+
+    const horizontalName1 = screen.getByTestId(HORIZONTALNAME1);
+    expect(horizontalName1).toBeInTheDocument();
+    expect(horizontalName1).toHaveTextContent('GG');
+
+    const horizontalShareBtn1 = screen.getByTestId('1-horizontal-share-btn');
+    expect(horizontalShareBtn1).toBeInTheDocument();
+
+    const horizontalFavoriteBtn1 = screen.getByTestId('1-horizontal-favorite-btn');
+    expect(horizontalFavoriteBtn1).toBeInTheDocument();
+  });
+  test('2 - check the all, food and drinks filters', () => {
+    renderWithRouterAndContext(<FavoriteRecipes />);
+
+    const horizontalName0 = screen.getByTestId(HORIZONTALNAME0);
+    expect(horizontalName0).toBeInTheDocument();
+    expect(horizontalName0).toHaveTextContent('Corba');
+
+    const horizontalName1 = screen.getByTestId(HORIZONTALNAME1);
+    expect(horizontalName1).toBeInTheDocument();
+    expect(horizontalName1).toHaveTextContent('GG');
+
+    const filterByFoodBtn = screen.getByTestId('filter-by-food-btn');
+    userEvent.click(filterByFoodBtn);
+    expect(horizontalName0).toBeInTheDocument();
+    expect(horizontalName0).toHaveTextContent('Corba');
+
+    expect(horizontalName1).not.toBeInTheDocument();
+
+    const filterByDrinkBtn = screen.getByTestId(FILTERBYDRINKBTN);
+    userEvent.click(filterByDrinkBtn);
+    expect(horizontalName0).toBeInTheDocument();
+    expect(horizontalName0).toHaveTextContent('GG');
+    expect(horizontalName1).not.toBeInTheDocument();
+
+    const filterByAllBtn = screen.getByTestId('filter-by-all-btn');
+    userEvent.click(filterByAllBtn);
+
+    expect(horizontalName0).toBeInTheDocument();
+    expect(horizontalName0).toHaveTextContent('Corba');
+    expect(horizontalName1).toHaveTextContent('GG');
+  });
+  test('3 - test if copy the link to share', async () => {
+    renderWithRouterAndContext(<FavoriteRecipes />);
+
+    const horizontalShareBtn0 = screen.getByTestId('0-horizontal-share-btn');
+    expect(horizontalShareBtn0).toBeInTheDocument();
+
+    userEvent.click(horizontalShareBtn0);
+    const shared = screen.getByText('Link copied!');
+    expect(shared).toBeInTheDocument();
+    expect(navigator.clipboard.writeText).toBeCalledTimes(1);
+    expect(navigator.clipboard.writeText).toHaveBeenCalledWith('http://localhost:3000/foods/52977');
+  });
+  test('4 - test if you unfavorite any recipe', () => {
+    renderWithRouterAndContext(<FavoriteRecipes />);
+
+    const horizontalName1 = screen.getByTestId(HORIZONTALNAME1);
+    expect(horizontalName1).toBeInTheDocument();
+    expect(horizontalName1).toHaveTextContent('GG');
+
+    const horizontalFavoriteBtn1 = screen.getByTestId('1-horizontal-favorite-btn');
+    expect(horizontalFavoriteBtn1).toBeInTheDocument();
+
+    userEvent.click(horizontalFavoriteBtn1);
+    expect(horizontalName1).not.toBeInTheDocument();
+  });
+  test('5 - tests if a recipe is unfavorable by filtering only drinks', () => {
+    renderWithRouterAndContext(<FavoriteRecipes />);
+
+    const filterByDrinkBtn = screen.getByTestId(FILTERBYDRINKBTN);
+    expect(filterByDrinkBtn).toBeInTheDocument();
+
+    userEvent.click(filterByDrinkBtn);
+
+    const horizontalName0 = screen.getByTestId(HORIZONTALNAME0);
+    expect(horizontalName0).toBeInTheDocument();
+    expect(horizontalName0).toHaveTextContent('GG');
+
+    const horizontalFavoriteBtn0 = screen.getByTestId('0-horizontal-favorite-btn');
+    expect(horizontalFavoriteBtn0).toBeInTheDocument();
+
+    userEvent.click(horizontalFavoriteBtn0);
+    expect(horizontalName0).not.toBeInTheDocument();
   });
 });
--- src/pages/FavoriteRecipes.jsx
+++ src/pages/FavoriteRecipes.jsx
@@ -1,24 +1,54 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
+import blackHeartIcon from '../images/blackHeartIcon.svg';
+import shareIcon from '../images/shareIcon.svg';
+
+const style = {
+  display: 'flex',
+  width: '220px',
+  flexWrap: 'wrap',
+  justifyContent: 'space-between',
+};
 
 export default function FavoriteRecipes() {
-  const [allRecipes] = useState(JSON
+  const [allRecipes, setAllRecipes] = useState(JSON
     .parse(localStorage.getItem('favoriteRecipes')));
   const [recipes, setRecipes] = useState(JSON
     .parse(localStorage.getItem('favoriteRecipes')));
+  const [shared, setShared] = useState();
+  const [filter, setFilter] = useState('all');
 
-  const filterAll = () => {
-    setRecipes(allRecipes);
+  useEffect(() => {
+    const myFilter = () => {
+      if (filter === 'all') {
+        setRecipes(allRecipes);
+        return;
+      }
+      const filtered = allRecipes.filter((r) => r.type === filter);
+      setRecipes(filtered);
+    };
+    myFilter();
+  }, [filter]);
+
+  const shareBtn = (type, id) => {
+    setShared(id);
+    navigator.clipboard.writeText(`http://localhost:3000/${type}s/${id}`);
   };
 
-  const filterFoods = () => {
-    const favoriteFoods = allRecipes.filter((r) => r.type === 'food');
-    setRecipes(favoriteFoods);
-  };
-
-  const filterDrinks = () => {
-    const favoriteDrinks = allRecipes.filter((r) => r.type === 'drink');
-    setRecipes(favoriteDrinks);
+  const removeFavorite = (id) => {
+    console.log(id);
+    const favoriteRecipes = JSON.parse(localStorage.getItem('favoriteRecipes'));
+    const newFavoriteRecipes = favoriteRecipes
+      .filter((r) => r.id !== id);
+    localStorage.setItem('favoriteRecipes', JSON.stringify(newFavoriteRecipes));
+    setAllRecipes(newFavoriteRecipes);
+    if (filter === 'all') {
+      setRecipes(newFavoriteRecipes);
+      return;
+    }
+    const filtered = newFavoriteRecipes.filter((r) => r.type === filter);
+    setRecipes(filtered);
   };
 
   return (
@@ -27,32 +57,71 @@ export default function FavoriteRecipes() {
       <button
         type="button"
         data-testid="filter-by-all-btn"
-        onClick={ filterAll }
+        onClick={ () => setFilter('all') }
       >
         All
       </button>
       <button
         type="button"
         data-testid="filter-by-food-btn"
-        onClick={ filterFoods }
+        onClick={ () => setFilter('food') }
       >
         Foods
       </button>
       <button
         type="button"
         data-testid="filter-by-drink-btn"
-        onClick={ filterDrinks }
+        onClick={ () => setFilter('drink') }
       >
         Drinks
       </button>
-      { recipes.map((r, i) => (
-        <div key={ i }>
-          <img
-            data-testid={ `${i}-horizontal-image` }
-            src={ r.image }
-            alt={ r.name }
-          />
-        </div>))}
+      <div style={ style }>
+        { recipes && recipes.map((r, index) => (
+          <div key={ index }>
+            <Link to={ `/${r.type}s/${r.id}` }>
+              <img
+                data-testid={ `${index}-horizontal-image` }
+                src={ r.image }
+                alt={ r.name }
+                style={ { width: '100px' } }
+              />
+            </Link>
+            <p
+              data-testid={ `${index}-horizontal-top-text` }
+              style={ { width: '100px' } }
+            >
+              { `${r.nationality} - ${r.category}
+                ${r.alcoholicOrNot}` }
+            </p>
+            <Link to={ `/${r.type}s/${r.id}` }>
+              <p
+                data-testid={ `${index}-horizontal-name` }
+                style={ { width: '100px' } }
+              >
+                { r.name }
+              </p>
+            </Link>
+            <button
+              type="button"
+              data-testid={ `${index}-horizontal-share-btn` }
+              src={ shareIcon }
+              label="favorite"
+              onClick={ () => shareBtn(r.type, r.id) }
+            >
+              <img src={ shareIcon } alt="share icon" />
+              { shared === r.id && <p>Link copied!</p>}
+            </button>
+            <button
+              type="button"
+              data-testid={ `${index}-horizontal-favorite-btn` }
+              src={ blackHeartIcon }
+              label="share"
+              onClick={ () => removeFavorite(r.id) }
+            >
+              <img src={ blackHeartIcon } alt="black heart icon" />
+            </button>
+          </div>))}
+      </div>
     </div>
   );
 }
