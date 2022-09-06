efbc692bb5faf04476b12674c651a27aa8bb9923
caiooliveirah09 <leelros@hotmail.com>
2022-09-06 14:22:58 -0300

    adicionei novo provider nos tests

--- src/utils/helpers/renderWithRouterAndContext.js
+++ src/utils/helpers/renderWithRouterAndContext.js
@@ -3,15 +3,18 @@ import { Router } from 'react-router-dom';
 import { createMemoryHistory } from 'history';
 import { render } from '@testing-library/react';
 import { RecipesProvider } from '../../context/RecipesContext';
+import Provider from '../../context/Provider';
 
 const renderWithRouterAndContext = (component, route = '/') => {
   const history = createMemoryHistory({ initialEntries: [route] });
   return ({
     ...render(
       <Router history={ history }>
+        <Provider />
         <RecipesProvider>
           {component}
         </RecipesProvider>
+        <Provider />
       </Router>,
     ),
     history,
